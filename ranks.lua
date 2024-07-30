228301184fbe1ba0864c48d2bad74fd92301e3d8
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-07-30 16:37:28 -0300

    Update ranks.lua

--- ranks.lua
+++ ranks.lua
@@ -1,47 +1,18 @@
-local RankTable = {
-["Creators"] = {
-			Level = 800;
-  		Users = {
-  				"b112014";-- 
-  				"nanaadef";
-  				"Vinioerro"; -- helper
-  				"CMBORISS"; -- helper
-  				"ZE_GOTINHA893"; -- helper
-  				"nanaadef"; -- helper teste
-  				"darkzinhoUAS"; -- helper
-  				"ff_mnt8";
-  				"joao03729";
-  				"sajeoeni"; --helper
-  				"pedrinyo6587"; --helper
-  				"EDUARDO87654322"; -- helper
-  				"mestrelout"; --helper
-  				"BielDeftones"; --helper
-  				"Red_Draggom"; -- Helper
-  				"menorbrayanxxx"; -- Helper
-  				"Lince_Destiny"; -- Helper
-  				"El_pepets3";-- Helper
-  				"Lekejshshshsh";-- Helper
-  				"j7po3";
-  				"Dells_lakash",
-  				"afog1998";-- Helper
-  				"meleidinha10_pcmega";
-  				"deathxzlord";
-  				"ed3vsrs5",
-			};
-		};
-		["Administração"] = {
-			Level = 850;
-			Users = {	
-				"Lucaslacruz"; 
-				"RaihNoCraK";
-				"A_Menor01";
-				"jguuh";
-				"ProzinhoBr23213";
-				"GojonoCB";
-			};
-		};
+{
+  "Creators": {
+    "Level": 800,
+    "Users": [
+      "b112014",
+      "nanaadef",
+      "Vinioerro",
+     
+    ]
+  },
+  "Administração": {
+    "Level": 850,
+    "Users": [
+      "Lucaslacruz",
+      "RaihNoCraK",
+    ]
+  }
 }
-
-return RankTable
-
-
